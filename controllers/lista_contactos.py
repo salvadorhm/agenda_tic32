0fcca0ad213a4832e195838407f8ebb638b1100a
Salvador HM <salvadorhm@mba.local>
2026-06-29 09:31:24 -0600

    CREATED consulta a tabla contactos

--- controllers/lista_contactos.py
+++ controllers/lista_contactos.py
@@ -11,12 +11,11 @@ class ListaContactos:
             conn = sqlite3.connect('sql/agenda.db')
             cursor = conn.cursor()
             # Consulta los registros de la tabla contactos
-            resultado = cursor.execute("SELECT * FROM contactos;")
-            print(f"RESULTADO: {resultado}")
-            
-            # Almacena los resultados en un diccionario
+            cursor.execute("SELECT * FROM contactos;")            
+            # Crea un array vacio para almacenar los registros
             contactos = []
-            for row in resultado.fetchall():
+            # Almacena cada registro en un diccionario
+            for row in cursor.fetchall():
                 contacto = {
                     'id_contacto': row[0],
                     'nombre': row[1],
@@ -25,7 +24,8 @@ class ListaContactos:
                     'email': row[4],
                     'telefono': row[5]
                 }
-                contactos[row[0]] = contacto
+                # Agrega el diccionario creado al array
+                contactos.append(contacto)
         
             # Cierra la conexión a la base de datos
             conn.close()
@@ -37,7 +37,8 @@ class ListaContactos:
         except Exception as error:
             print(f"ERROR 101: {error.args}")
             return []
-
+        finally:
+            conn.close()
 
 
     def GET(self):
